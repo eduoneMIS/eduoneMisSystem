8c2a64a2d5a7e3b7b6f0060ae4e0386774f051d8
EduOneMIS_Dev <istudyjs019@gmail.com>
2023-05-09 19:12:48 +0800

    v1.1.2

--- QA.js
+++ QA.js
@@ -23,10 +23,11 @@ function all_only() {
 
 // 單一電腦
 function only() {
-    big_title.textContent = `請確認您的帳密是否輸入正確`;
+    big_title.innerHTML = `<i class='bx bx-user-circle' ></i>
+    <h2 class="title-text">請確認您的帳密是否輸入正確</h2>`;
     qustion.innerHTML = `
-    <button class="choose" id="done">我解決了</button>
-    <button class="choose" id="true">帳號沒錯</button>
+    <button class="choose" id="done"><i class='bx bx-like'></i><p>我解決了</p></button>
+    <button class="choose" id="true"><i class='bx bx-check'></i><p>帳號沒錯</p></button>
     `;
     const true_BTN = document.getElementById("true");
     var done_BTN = document.getElementById("done");
@@ -43,11 +44,12 @@ function only() {
 
 // 全館電腦
 function all() {
-    big_title.textContent = `請確認NAS主機以及容器是否開啟`;
+    big_title.innerHTML = `<i class='bx bxl-docker'></i>
+    <h2 class="title-text">請確認NAS主機以及容器是否開啟</h2>`;
     qustion.innerHTML = `
-    <button class="choose" id="done">我解決了</button>
-    <button class="choose" id="cant">無法開機</button>
-    <button class="choose" id="On">已開啟</button>
+    <button class="choose" id="done"><i class='bx bx-like'></i><p>我解決了</p></button>
+    <button class="choose" id="cant"><i class='bx bx-bug-alt'></i><p>無法開機</p></button>
+    <button class="choose" id="On"><i class='bx bx-check'></i><p>已開啟</p></button>
     `;
     note.innerHTML = `技術支援網站 - <a href=https://hackmd.io/@eduone-imstudy-MIS/r1X0il1-n#%E7%8B%80%E6%B3%81%E4%BA%8C-NAS%E5%AE%B9%E5%99%A8%E6%B2%92%E6%9C%89%E9%96%8B-">NAS容器沒有開</a>`
     const on_BTN = document.getElementById("On");
@@ -71,10 +73,11 @@ function all() {
 
 // 更換軟體
 function reset_software() {
-    big_title.textContent = `請您更換Study123軟體檔案`;
+    big_title.innerHTML = `<i class='bx bx-download'></i>
+    <h2 class="title-text">請您重新安裝Study123軟體檔案</h2>`;
     qustion.innerHTML = `
-    <button class="choose" id="done">已解決</button>
-    <button class="choose" id="reset">已更換</button>
+    <button class="choose" id="done"><i class='bx bx-like'></i><p>已解決</p></button>
+    <button class="choose" id="reset"><i class='bx bx-check'></i><p>已重新安裝</p></button>
     `;
     var done_BTN = document.getElementById("done");
     const reset_BTN = document.getElementById("reset");
@@ -91,10 +94,11 @@ function reset_software() {
 }
 
 function configIP() {
-    big_title.textContent = `確認您得study123資料夾的config.txt IP`;
+    big_title.innerHTML = `<i class='bx bx-planet' ></i>
+    <h2 class="title-text">確認您得study123資料夾的config.txt IP</h2>`
     qustion.innerHTML = `
-    <button class="choose" id="done">已解決</button>
-    <button class="choose" id="setted">已更換本館IP</button>
+    <button class="choose" id="done"><i class='bx bx-like'></i><p>已解決</p></button>
+    <button class="choose" id="setted"><i class='bx bx-check'></i><p>已更換本館IP</p></button>
     `;
     var done_BTN = document.getElementById("done");
     const setted_BTN = document.getElementById("setted");
@@ -112,15 +116,32 @@ function configIP() {
 
 
 
-
 function fail() {
-    big_title.textContent = `非常遺憾`;
-    qustion.innerHTML = `您的問題須由專人為您處理`;
-    note_fomate.textContent = `請於【每周一至周五 9:30AM ~ 9:00PM】聯繫 _ 金鑫資訊部`
+    big_title.innerHTML = `<i class='bx bxs-no-entry'></i>
+    <h2 class="title-text">非常抱歉</h2>`;
+    qustion.innerHTML = `<p class="prompt-message">您的問題預料之外，涉及到專業領域須由工程師為您解決</p>`;
+    note_fomate.textContent = `請於【每周一至周五 9:30AM ~ 9:00PM】聯繫 - 金鑫資訊部`
 }
 // 已完成
 function done() {
-    big_title.textContent = `您已完成基本除錯`;
-    qustion.innerHTML = ``;
+    big_title.innerHTML = `<i class='bx bx-like'></i>
+    <h2 class="title-text">您已完成基本除錯</h2>`;
+    qustion.innerHTML = `<p class="prompt-message">恭喜您</p>`;
     note_fomate.textContent = `感謝您使用資訊助手`
 }
+
+
+
+
+// -------- MENU --------
+const homeBTN = document.getElementById("go-home");
+// const backBTN = document.getElementById("go-back");
+homeBTN.addEventListener("click", function (event) {
+    event.preventDefault();
+    location.reload();
+});
+
+// backBTN.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     location.reload();
+// });
